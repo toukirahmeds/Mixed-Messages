d7545b046ff83a4f1796f946c5b86ec960485652
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2022-03-11 21:40:45 +0600

    all messages array create

---
+++ index.js
@@ -0,0 +1,8 @@
+const allMessages = [
+  'Hi buddy! Hope you are doing great!',
+  'Congratulations for being able to run the program.',
+  'Welcome to our application.',
+  'You are going to win.',
+  'Happy Coding',
+  'Awesome! So you are doing a great progress!'
+]
